be9d333a00b17b196722cdacf7ab04ceea39cffd
melvinren <melvin_ren@icloud.com>
2017-08-14 22:15:13 +0800

    router

---
+++ react/learning_logs/src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component{
+	render(){
+		return (
+			<div>
+				<p>Learning Log</p>
+				<p>Learning log helps you keep track of your learning, for any topic you''re learning about</p>
+			</div>
+			);
+	}
+}
+
+export default Home;
